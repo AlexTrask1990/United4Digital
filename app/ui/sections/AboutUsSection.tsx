52d66470cdcf7c2328f8c336e886cc42327988d1
Daniil <portal.sid.grand2@gmail.com>
2024-05-23 16:11:40 +0300

    translate-z-0.

--- app/ui/sections/AboutUsSection.tsx
+++ app/ui/sections/AboutUsSection.tsx
@@ -4,6 +4,38 @@ import SectionHeading from "@/app/ui/sections/components/Heading/SectionHeading"
 import Container from "@/app/ui/Container/Container";
 import SectionWrapper from "@/app/ui/SectionWrapper/SectionWrapper";
 import AboutUsLottie from "@/app/ui/sections/components/LottieImage/AboutUsLottie";
+import { IOfferList } from "@/app/types/definitions";
+
+const offerList: IOfferList[] = [
+  {
+    src: "/offerIconOne.svg",
+    alt: "Offer comprehensive solution",
+    text: `At United4Digital, we offer comprehensive solutions tailored to innovate
+  the dynamic landscape of digital marketing. Whether through our
+  proprietary video streaming applications or exclusive exchanges for
+  advertisers and publishers, we strive to forge a seamless synergy
+  between our clients' objectives and their audience. Our focus lies in
+  managing the entirety of the advertising framework, digital
+  monetization, and strategic planning with a cohesive approach.`,
+  },
+  {
+    src: "/offerIconTwo.svg",
+    alt: "We specialize in providing scalable",
+    text: `We specialize in providing scalable, high quality, and trusted
+  inventory across various platforms including Display, Connected TV,
+  InApp, and Audio. Through meticulous selection processes, we handpick top-tier publishers and emerging high-quality content to
+  ensure that all our partners deliver a valuable advertising experience.
+  `,
+  },
+  {
+    src: "/offerIconThree.svg",
+    alt: "United4Digital is committed to maintaining the utmost quality",
+    text: `United4Digital is committed to maintaining the utmost quality of
+  traffic through strategic partnerships with leading data transparency
+  providers and anti-fraud protection systems. Our dedication to excellence ensures that our clients receive nothing short of the best
+  in traffic quality.`,
+  },
+];
 
 export default function AboutUsSection() {
   return (
@@ -33,39 +65,21 @@ export default function AboutUsSection() {
           </div>
         </Container>
         <Container>
-          <div className="laptop:pt-20 pt-10 laptop:flex">
-            <OfferCard
-              src={"/offerIconOne.svg"}
-              alt={"Offer comprehensive solution"}
-              text={`At United4Digital, we offer comprehensive solutions tailored to innovate
-                    the dynamic landscape of digital marketing. Whether through our
-                    proprietary video streaming applications or exclusive exchanges for
-                    advertisers and publishers, we strive to forge a seamless synergy
-                    between our clients' objectives and their audience. Our focus lies in
-                    managing the entirety of the advertising framework, digital
-                    monetization, and strategic planning with a cohesive approach.`}
-            />
-            <OfferCard
-              src={"/offerIconTwo.svg"}
-              alt={"We specialize in providing scalable"}
-              text={`We specialize in providing scalable, high quality, and trusted
-              inventory across various platforms including Display, Connected TV,
-              InApp, and Audio. Through meticulous selection processes, we handpick top-tier publishers and emerging high-quality content to
-              ensure that all our partners deliver a valuable advertising experience.`}
-              className={"mt-6 laptop:mt-0 laptop:ml-6"}
-            />
-
-            <OfferCard
-              src={"/offerIconThree.svg"}
-              alt={
-                "United4Digital is committed to maintaining the utmost quality"
+          <div className="laptop:pt-20 pt-10 laptop:flex translate-z-0">
+          {_.map(
+              offerList,
+              ({ src, alt, text }: IOfferList, index: number) => {
+                return (
+                  <OfferCard
+                    key={alt}
+                    src={src}
+                    alt={alt}
+                    text={text}
+                    className={`${index && "mt-6 laptop:mt-0 laptop:ml-6 translate-z-0"}`}
+                  />
+                );
               }
-              text={`United4Digital is committed to maintaining the utmost quality of
-              traffic through strategic partnerships with leading data transparency
-              providers and anti-fraud protection systems. Our dedication to excellence ensures that our clients receive nothing short of the best
-              in traffic quality.`}
-              className={"mt-6 laptop:mt-0 laptop:ml-6"}
-            />
+            )}
           </div>
         </Container>
       </div>
--- app/ui/sections/components/OfferCard/OfferCard.tsx
+++ app/ui/sections/components/OfferCard/OfferCard.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 export default function OfferCard({ src, alt, text, className }: IProps) {
   return (
-    <div className={`bg-white py-8 px-10 rounded-lg flex flex-col items-center laptop:block ${className}`}>
+    <div className={`bg-white py-8 px-10 rounded-lg flex flex-col items-center laptop:block ${className} translate-z-0`}>
       <Image src={src} width={0} height={0} alt={alt} className="w-[100px] h-auto laptop:w-auto"/>
       <p className="pt-10">{text}</p>
     </div>
