46cce47200c736ce6302db19bbe66c2379f3534a
Daniil <portal.sid.grand2@gmail.com>
2024-05-29 11:22:39 +0300

    test v5

--- app/ui/sections/components/OfferCard/OfferCard.tsx
+++ app/ui/sections/components/OfferCard/OfferCard.tsx
@@ -11,7 +11,7 @@ interface IProps {
 export default function OfferCard({ src, alt, text, className }: IProps) {
   return (
     <div className={`bg-white py-8 px-10 rounded-lg flex flex-col items-center laptop:block ${className}`}>
-      <Image src={src} width={100} height={100} alt={alt} className="w-auto h-auto laptop:w-auto"/>
+      <Image src={src} width={100} height={100} alt={alt} className="w-[100px] h-auto laptop:w-auto"/>
       <p className="pt-10">{text}</p>
     </div>
   );
