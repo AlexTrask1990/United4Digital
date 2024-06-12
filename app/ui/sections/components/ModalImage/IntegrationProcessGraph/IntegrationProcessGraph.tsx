af88d19816493bc4ef6ef256b8c08de7a4971025
Daniil <portal.sid.grand2@gmail.com>
2024-06-12 19:01:31 +0300

    fix img

--- app/ui/sections/components/ModalImage/IntegrationProcessGraph/IntegrationProcessGraph.tsx
+++ app/ui/sections/components/ModalImage/IntegrationProcessGraph/IntegrationProcessGraph.tsx
@@ -23,9 +23,9 @@ export default function IntegrationProcessGraph() {
       />
       <Image
         alt="Flowchart depicting the integration process"
-        src="/content/integration-process.png"
-        width={1010}
-        height={490}
+        src="/content/integration-process.svg"
+        width={0}
+        height={0}
         className="w-auto h-auto cursor-zoom-in hidden laptop:flex"
         onClick={handleClick}
       />
