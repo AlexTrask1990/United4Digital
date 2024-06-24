0a2a16745887b6b7e91b4ad40cbabe314e5723d2
Daniil <portal.sid.grand2@gmail.com>
2024-06-24 19:37:41 +0300

    email.secure.net

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
     const message = _.get(data, "message", "Some message");
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.office365.com",
+      host: "email.secureserver.net",
       port: 587,
       secure: false,
       auth: {
