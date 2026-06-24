7c8ab97aa90df909f97bc2d65d68225f548f17c7
Даниил Заглядов <portal.sid.grand2@gmail.com>
2026-06-24 19:34:22 +0300

    HUpate2

--- app/ui/sections/PresentSection.tsx
+++ app/ui/sections/PresentSection.tsx
@@ -3,7 +3,7 @@ import LogoLottie from "./components/LottieImage/LogoLottie";
 export default function PresentSection() {
   return (
     <section className="bg-primary flex h-[calc(100vh-85px)] flex-col items-center justify-center overflow-hidden">
-      <div className="min-h-[320px] max-w-[1280px] overflow-hidden laptop:min-h-[800px]">
+      <div className="min-h-[360px] max-w-[1280px] overflow-hidden laptop:min-h-[880px]">
         <LogoLottie />
       </div>
     </section>
